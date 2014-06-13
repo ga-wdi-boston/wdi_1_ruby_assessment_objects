35228463680f271d9d87b0648a94e1a9b50ad068
sarahchristine913 <s.mele13@gmail.com>
2014-06-13 14:16:02 -0400

    Mele_Sarah Completed Assessment Objects
    * Pretty sure that I'm solid on all of this, maybe need slight clarification about the internal location of instance variables within the class, we'll see.

--- objects.rb
+++ objects.rb
@@ -1,25 +1,75 @@
 #### Question 1
 # Define a class called `Pet`, and a class `Dog` that inherits from `Pet`.
+class Pet
+end
 
+class Dog < Pet
+end
 
 #### Question 2
 # Copy your class definitions from question 1 here.
 # Then add an initializer to the `Pet` class that accepts a "name" argument
 # and stores the value as an instance variable.
 
+class Pet
+
+attr_accessor :name
+
+  def initialize(:name)
+    @name = name
+  end
+end
+
+class Dog < Pet
+
+end
 
 #### Question 3
 # Copy your class definitions from question 2 here.
 # Then add an instance method called `bark` to the `Dog` class that returns the
 # string "bark bark".
 
+class Pet
+
+  attr_accessor :name
+
+  def initialize(:name)
+    @name = name
+  end
+end
+
+
+class Dog < Pet
+
+  attr_accessor :name :bark
+
+    @bark = bark    #not sure if this is correct?? its not in a method in the class
+
+  def bark
+    "bark bark"
+  end
+
+end
+
 
 #### Question 4
 # Write code that would create a new instance of the `Dog` class with the
 # name "Lassie", and call the `bark` method on it.
-
+Dog.new("Lassie").bark
 
 #### Question 5
 # Write code that would create a new instance of the `Pet` class with the
 # name "Reginald". Assuming the `Pet` class had a setter for "name", write
 # code that would change Reginald's name to "Snookums".
+
+hamster = Pet.new("Reginald")
+
+hamster.name("Snookums")
+
+
+
+
+
+
+
+
