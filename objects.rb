04e0e109d230e509d9abf466436f6417488e7fe5
Jonathan Symond <jsyzer@Jonathans-MacBook-Air.local>
2014-09-26 10:28:48 -0400

    1/5

--- objects.rb
+++ objects.rb
@@ -14,6 +14,16 @@ end
 end
 
 
+##BOARD ANSWER
+
+class Pet
+end
+
+class Dog < Pet
+end
+
+
+
 #### Question 2
 # Copy your class definitions from question 1 here.
 # Then add an initializer to the `Pet` class that accepts a "name" argument
@@ -36,13 +46,39 @@ class Dog < Pet
 end
 end
 
+# BOARD ANSWER
+
+class Pet
+def initialize(name)
+  @name = name
+end
+end
+
+class Dog < Pet
+end
+
+
 
 #### Question 3
 # Copy your class definitions from question 2 here.
 # Then add an instance method called `bark` to the `Dog` class that returns the
 # string "bark bark".
 
-# I am struggling with these questions but I feel much better about yesterday's quiz, methods, and using Git at this time.
+# BOARD ANSWER
+
+class Pet
+def initialize(name)
+  @name = name
+end
+end
+
+class Dog < Pet
+  "bark bark"
+end
+end
+
+# don't always need attr_accessor
+
 
 
 
@@ -50,8 +86,18 @@ end
 # Write code that would create a new instance of the `Dog` class with the
 # name "Lassie", and call the `bark` method on it.
 
+dog = Dog.new "Lassie"
+dog.bark
+
 
 #### Question 5
 # Write code to create a new array containing 3 dog names (as strings).
 # Then write code to add the string "is a good dog!" to every name.
 # Save the result in a new array, but don't output anything to the Terminal.
+
+names = ["Fido", "Sammie", "Toby"]
+new_names = names.map do |name|
+  name + " is a good dog!"
+end
+
+# I thought this was an object question, but this map topic makes sense from yesterday's quiz.
