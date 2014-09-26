dbf795280e053de948abcb840cfc582659a7e6f9
MDROR <dymmchl@yahoo.com>
2014-09-26 09:35:53 -0400

    finished quiz

--- objects.rb
+++ objects.rb
@@ -1,25 +1,73 @@
 #### Question 1
 # Define a class called `Pet`, and a class `Dog` that inherits from `Pet`.
+class Pet
+end
 
+class Dog << Pet
+end
 
 #### Question 2
 # Copy your class definitions from question 1 here.
 # Then add an initializer to the `Pet` class that accepts a "name" argument
 # and stores the value as an instance variable.
+class Pet
+attr_acessor :name
+  @name
+
+end
+
+class Dog << Pet
+end
 
 
 #### Question 3
 # Copy your class definitions from question 2 here.
 # Then add an instance method called `bark` to the `Dog` class that returns the
 # string "bark bark".
+class Pet
+attr_acessor :name
+  @name
+
+def bark
+  puts "bar bark"
+
+end
+
+class Dog << Pet
+end
 
 
 #### Question 4
 # Write code that would create a new instance of the `Dog` class with the
 # name "Lassie", and call the `bark` method on it.
+class Pet
+attr_acessor :name
+  @name
+
+def bark
+  puts "bar bark"
+
+end
+
+class Dog << Pet
+end
+
+Lassie = Dog.new
+
+Lassie.bark
 
 
 #### Question 5
 # Write code to create a new array containing 3 dog names (as strings).
-# Then write code to add the string "is a good dog!" to every name. 
+# Then write code to add the string "is a good dog!" to every name.
 # Save the result in a new array, but don't output anything to the Terminal.
+
+doggies = ["spot", "fido", "dino"]
+
+doggies.map! do |dog|
+
+  puts dog + "is a good dog!"
+
+end
+
+
