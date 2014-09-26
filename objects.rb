286493b845f1b0ca93f5b197f3f598e1287f033e
shaynaCummings <shayna.cummings@gmail.com>
2014-09-26 09:44:16 -0400

    submitting quiz

--- objects.rb
+++ objects.rb
@@ -1,11 +1,30 @@
 #### Question 1
 # Define a class called `Pet`, and a class `Dog` that inherits from `Pet`.
 
+class Pet
+  attr_accessor :name
+end
 
-#### Question 2
-# Copy your class definitions from question 1 here.
-# Then add an initializer to the `Pet` class that accepts a "name" argument
-# and stores the value as an instance variable.
+class Dog < Pet
+end
+
+
+# #### Question 2
+# # Copy your class definitions from question 1 here.
+# # Then add an initializer to the `Pet` class that accepts a "name" argument
+# # and stores the value as an instance variable.
+
+class Pet
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+end
+
+class Dog < Pet
+end
 
 
 #### Question 3
@@ -13,13 +32,47 @@
 # Then add an instance method called `bark` to the `Dog` class that returns the
 # string "bark bark".
 
+class Pet
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+end
+
+class Dog < Pet
+
+  def initialize(name)
+    super(name)
+  end
+
+  def bark
+    return "bark bark"
+  end
+
+end
+
 
 #### Question 4
 # Write code that would create a new instance of the `Dog` class with the
 # name "Lassie", and call the `bark` method on it.
 
+new_dog = Dog.new("Lassie")
+new_dog.bark
 
 #### Question 5
 # Write code to create a new array containing 3 dog names (as strings).
-# Then write code to add the string "is a good dog!" to every name. 
+# Then write code to add the string "is a good dog!" to every name.
 # Save the result in a new array, but don't output anything to the Terminal.
+
+dog_array = []
+@lassie = Dog.new("Lassie")
+@bowser = Dog.new("Bowser")
+@lilly = Dog.new("Lilly")
+
+dog_array.push(@lilly.name, @bowser.name, @lassie.name)
+
+new_array = dog_array.map do |name|
+             "#{name} is a good dog!"
+            end
