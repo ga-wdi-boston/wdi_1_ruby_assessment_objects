0a3ad7e3a472f80bdff32d652467601c814e47d1
IljaGoushcha <iljagou@yahoo.com>
2014-09-26 09:47:10 -0400

    Class quiz done

--- objects.rb
+++ objects.rb
@@ -1,25 +1,61 @@
 #### Question 1
 # Define a class called `Pet`, and a class `Dog` that inherits from `Pet`.
 
+# class Pet
+# end
+
+# class Dog < Pet
+# end
 
 #### Question 2
 # Copy your class definitions from question 1 here.
 # Then add an initializer to the `Pet` class that accepts a "name" argument
 # and stores the value as an instance variable.
+# class Pet
+#   attr_accessor(:name)
+#   def initialize(name)
+#     @name = name
+#   end
+# end
 
+# class Dog < Pet
+# end
 
+# my_pet = Pet.new("Porky")
+# puts my_pet.name
 #### Question 3
 # Copy your class definitions from question 2 here.
 # Then add an instance method called `bark` to the `Dog` class that returns the
 # string "bark bark".
+class Pet
+  attr_accessor(:name)
+  def initialize(name)
+    @name = name
+  end
+end
 
+class Dog < Pet
+  super (name)
+  def bark
+    puts "bark"
+  end
+end
 
+my_pet = Pet.new("Porky")
+puts my_pet.name
 #### Question 4
 # Write code that would create a new instance of the `Dog` class with the
 # name "Lassie", and call the `bark` method on it.
 
+my_dog = Gog.new("Lassie")
+my_dog.bark
+
 
 #### Question 5
 # Write code to create a new array containing 3 dog names (as strings).
-# Then write code to add the string "is a good dog!" to every name. 
+# Then write code to add the string "is a good dog!" to every name.
 # Save the result in a new array, but don't output anything to the Terminal.
+
+dogs ["Butcher", "Killer", "Fluffy"]
+
+new_dogs = dogs.map { |dog| dog + " is a good dog" }
