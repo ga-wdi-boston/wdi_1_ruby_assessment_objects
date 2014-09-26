749487eb23179ca0361fa975d0b3dbc381f87246
IljaGoushcha <iljagou@yahoo.com>
2014-09-26 10:35:22 -0400

    corrected my quiz yet again

--- objects.rb
+++ objects.rb
@@ -1,32 +1,19 @@
 #### Question 1
 # Define a class called `Pet`, and a class `Dog` that inherits from `Pet`.
 
-# class Pet
-# end
+# I got this one correct the first time
+class Pet
+end
 
-# class Dog < Pet
-# end
+class Dog < Pet
+end
 
 #### Question 2
 # Copy your class definitions from question 1 here.
 # Then add an initializer to the `Pet` class that accepts a "name" argument
 # and stores the value as an instance variable.
-# class Pet
-#   attr_accessor(:name)
-#   def initialize(name)
-#     @name = name
-#   end
-# end
 
-# class Dog < Pet
-# end
-
-# my_pet = Pet.new("Porky")
-# puts my_pet.name
-#### Question 3
-# Copy your class definitions from question 2 here.
-# Then add an instance method called `bark` to the `Dog` class that returns the
-# string "bark bark".
+# I got this one correct the first time
 class Pet
   attr_accessor(:name)
   def initialize(name)
@@ -35,18 +22,36 @@ class Pet
 end
 
 class Dog < Pet
-  super (name)
+end
+# my_pet = Pet.new("Porky")
+# puts my_pet.name
+
+#### Question 3
+# Copy your class definitions from question 2 here.
+# Then add an instance method called `bark` to the `Dog` class that returns the
+# string "bark bark".
+
+# I got this one correct the first time, except I might have left an unnecessary super method
+class Pet
+  attr_accessor(:name)
+  def initialize(name)
+    @name = name
+  end
+end
+
+class Dog < Pet
   def bark
     puts "bark"
   end
 end
+# my_pet = Pet.new("Porky")
+# puts my_pet.name
 
-my_pet = Pet.new("Porky")
-puts my_pet.name
 #### Question 4
 # Write code that would create a new instance of the `Dog` class with the
 # name "Lassie", and call the `bark` method on it.
 
+# I got this one correct the first time
 my_dog = Gog.new("Lassie")
 my_dog.bark
 
@@ -56,6 +61,6 @@ my_dog.bark
 # Then write code to add the string "is a good dog!" to every name.
 # Save the result in a new array, but don't output anything to the Terminal.
 
+# I got this one correct the first time
 dogs ["Butcher", "Killer", "Fluffy"]
-
-new_dogs = dogs.map { |dog| dog + " is a good dog" }
+new_dogs = dogs.map { |dog| dog + " is a good dog" } # I am not sure if this syntax is correct
