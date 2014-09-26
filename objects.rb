0333d07401923fe464c517b7ae7fff8b8a180736
Billy1339 <william.m.sargent@gmail.com>
2014-09-26 10:30:49 -0400

    i got got 2 or 3 out of 5 right. i added more than needed on many problems

--- objects.rb
+++ objects.rb
@@ -1,18 +1,28 @@
 #### Question 1
 # Define a class called `Pet`, and a class `Dog` that inherits from `Pet`.
 
-require 'pry'
+# require 'pry'
+#my code
+# class Pet
+
+#   attr_accessor :
+
+#   def initialize
+#   end
+# end
+
+# class Dog < Pet
+
+# end
+
+
+
+#better code below
 
 class Pet
-
-  attr_accessor :
-
-  def initialize
-  end
 end
 
 class Dog < Pet
-
 end
 
 
@@ -22,73 +32,129 @@ end
 # Then add an initializer to the `Pet` class that accepts a "name" argument
 # and stores the value as an instance variable.
 
-class Pet
 
-  attr_accessor :name
+#my code
+# class Pet
+
+#   attr_accessor :name
+
+#   def initialize(name)
+#     @name = name
+
+#   end
+# end
+
+
+#better code below
+class Pet
 
   def initialize(name)
     @name = name
-
   end
 end
 
+class Dog < Pet
+end
+
+my_pet = Pet.new("David")
+puts my_pet.name #doesnt work without getter method
 
 #### Question 3
 # Copy your class definitions from question 2 here.
 # Then add an instance method called `bark` to the `Dog` class that returns the
 # string "bark bark".
 
-class Pet
+#my code
+# class Pet
 
-  attr_accessor :name
+#   attr_accessor :name
+
+#   def initialize(name)
+#     @name = name
+
+#   end
+# end
+
+# class Dog < Pet
+
+#   attr_accessor :bark
+
+#   def bark
+#     @bark = "bark bark"
+#   end
+# end
+
+class Pet
 
   def initialize(name)
     @name = name
-
   end
 end
 
 class Dog < Pet
 
-  attr_accessor :bark
 
   def bark
-    @bark = "bark bark"
+    "bark bark"
   end
+
+
 end
 
 #### Question 4
 # Write code that would create a new instance of the `Dog` class with the
 # name "Lassie", and call the `bark` method on it.
 
-class Pet
+#my code below
+# class Pet
 
-  attr_accessor :name
+#   attr_accessor :name
+
+#   def initialize(name)
+#     @name = name
+
+#   end
+# end
+
+# class Dog < Pet
+
+#   attr_accessor :bark
+
+#   def initialize
+#     super(name)
+#   end
+
+#   def bark
+#     @bark = "bark bark"
+#   end
+# end
+# # binding.pry
+# new_dog = Dog.new "Lassie"
+# new_dog.bark
+
+
+# puts new_dog
+
+#better code
+class Pet
 
   def initialize(name)
     @name = name
-
   end
 end
 
 class Dog < Pet
 
-  attr_accessor :bark
-
-  def initialize
-    super(name)
-  end
 
   def bark
-    @bark = "bark bark"
+    "bark bark"
   end
+
+
 end
-binding.pry
-new_dog = Dog.new
-new_dog.bark
 
-
-puts new_dog
+dog = Dog.new "Lassie"
+dog.bark
 
 
 
