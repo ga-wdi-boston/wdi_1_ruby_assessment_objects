9c0c855772ed614256c79258e421165a4adf6dd1
mattmcdevitt93 <mattmcdevitt93@gmail.com>
2014-09-26 09:46:51 -0400

    friday quiz

--- objects.rb
+++ objects.rb
@@ -1,12 +1,34 @@
-#### Question 1
-# Define a class called `Pet`, and a class `Dog` that inherits from `Pet`.
+# #### Question 1
+# # Define a class called `Pet`, and a class `Dog` that inherits from `Pet`.
 
+class Pet
 
-#### Question 2
-# Copy your class definitions from question 1 here.
-# Then add an initializer to the `Pet` class that accepts a "name" argument
-# and stores the value as an instance variable.
+end
 
+class Dog < Pet
+def initialize
+ super Pet
+end
+end
+
+# #### Question 2
+# # Copy your class definitions from question 1 here.
+# # Then add an initializer to the `Pet` class that accepts a "name" argument
+# # and stores the value as an instance variable.
+
+class Pet
+
+def initialize (name)
+  @name = name
+end
+
+end
+
+class Dog < Pet
+def initialize
+ super Pet(name)
+end
+end
 
 #### Question 3
 # Copy your class definitions from question 2 here.
@@ -14,12 +36,42 @@
 # string "bark bark".
 
 
+class Pet
+
+attr_accessor :name
+
+def initialize (name)
+  @name = name
+end
+
+end
+
+class Dog < Pet
+
+  attr_accessor :name
+
+def initialize (name)
+ super Pet
+ @name = name
+end
+
+ def bark
+   return "bark bark"
+ end
+
+end
+
 #### Question 4
 # Write code that would create a new instance of the `Dog` class with the
 # name "Lassie", and call the `bark` method on it.
 
+dog = Dog.new("lassie")
+
+woof = Dog.bark
 
 #### Question 5
 # Write code to create a new array containing 3 dog names (as strings).
-# Then write code to add the string "is a good dog!" to every name. 
+# Then write code to add the string "is a good dog!" to every name.
 # Save the result in a new array, but don't output anything to the Terminal.
+
+
