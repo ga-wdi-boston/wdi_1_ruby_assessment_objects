b09054cccc8add3e1f2660153327123f73fb5c5e
Lauren Fazah <laurenfazah@gmail.com>
2014-09-26 09:31:21 -0400

    saving progress

--- objects.rb
+++ objects.rb
@@ -1,11 +1,27 @@
 #### Question 1
 # Define a class called `Pet`, and a class `Dog` that inherits from `Pet`.
 
+class Pet
+end
+
+class Dog < Pet
+end
+
+
 
 #### Question 2
 # Copy your class definitions from question 1 here.
 # Then add an initializer to the `Pet` class that accepts a "name" argument
 # and stores the value as an instance variable.
+
+class Pet
+  def initialize(name)
+    @name = name
+end
+
+class Dog < Pet
+end
+
 
 
 #### Question 3
@@ -21,5 +37,5 @@
 
 #### Question 5
 # Write code to create a new array containing 3 dog names (as strings).
-# Then write code to add the string "is a good dog!" to every name. 
+# Then write code to add the string "is a good dog!" to every name.
 # Save the result in a new array, but don't output anything to the Terminal.
