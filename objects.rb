1b9708d12cce8a285f02c1f4b1e374a405ee19fa
Lauren Fazah <laurenfazah@gmail.com>
2014-09-26 09:39:01 -0400

    all done

--- objects.rb
+++ objects.rb
@@ -17,11 +17,11 @@ end
 class Pet
   def initialize(name)
     @name = name
+  end
 end
 
 class Dog < Pet
 end
-
 
 
 #### Question 3
@@ -29,13 +29,42 @@ end
 # Then add an instance method called `bark` to the `Dog` class that returns the
 # string "bark bark".
 
+class Pet
+  def initialize(name)
+    @name = name
+  end
+end
+
+class Dog < Pet
+  attr_accessor :bark
+
+  def initialize(name)
+    @name = name
+    @bark = bark
+  end
+
+  def bark
+    "bark bark"
+  end
+end
 
 #### Question 4
 # Write code that would create a new instance of the `Dog` class with the
 # name "Lassie", and call the `bark` method on it.
 
+new_dog = Dog.new("Lassie").bark
 
 #### Question 5
 # Write code to create a new array containing 3 dog names (as strings).
 # Then write code to add the string "is a good dog!" to every name.
 # Save the result in a new array, but don't output anything to the Terminal.
+
+dog_names = ["Pup", "Penny", "Boomer"]
+
+good_puppies = dog_names.map do |pup|
+                 pup + " is a good dog!"
+               end
+
+puts good_puppies
+
+
